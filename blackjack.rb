95ba2e16dc85ac9b2cd9d61763f09077da23fce6
rachelbookhout <rbookhou@gmail.com>
2014-09-03 17:06:38 -0400

    added dealer

--- blackjack.rb
+++ blackjack.rb
@@ -1,6 +1,8 @@
 require 'pry'
 
 class Deck
+  attr_reader :cards
+
   def initialize
     @cards = build_deck
   end
@@ -8,26 +10,27 @@ class Deck
   def build_deck
     cards = []
 
-    suits = ["♠","♥","♦" "♣"]
+    suits = ["♠", "♥", "♦", "♣"]
     ranks = [2,3,4,5,6,7,8,9,10,"J","Q","K","A"]
 
     suits.each do |suit|
       ranks.each do |rank|
-        @deck << Card.new(rank, suit)
+        cards << Card.new(rank, suit)
       end
     end
 
     cards.shuffle!
   end
 
-  def draw
-    @deck.pop
+  def deal(hand)
+    # binding.pry
+    hand.cards << cards.pop
   end
 end
 
 
 class Card
-  attr_reader :rank
+  attr_reader :rank,:suit
   def initialize(rank, suit)
     @rank = rank
     @suit = suit
@@ -47,51 +50,102 @@ class Card
 end
 
 class Hand
- #want to take this from the draw function in the deck
-  def initialize
-    card1 = Card.new
-    card2 = Card.new
+  attr_reader :cards
+
+  def initialize(cards = [])
+    @cards = cards
   end
 
-  def intial_score
-  initial_hand = []
-  initial_hand << card1.value_of_card
-  initial_hand << card2.value_of_card
-  first_hand_score= initial_hand.inject(0){|result,element| result + element}
-  first_hand_score
+  # def intial_score
+  #   initial_hand = []
+  #   initial_hand << card1.value_of_card
+  #   initial_hand << card2.value_of_card
+  #   first_hand_score= initial_hand.inject(0){|result,element| result + element}
+  #   first_hand_score
+  # end
+
+  # def new_score
+  #   new_hand = initial_hand
+  #   current_score = new_hand.inject(0){|result,element| result + element}
+  # end
+
+  def score(hand)
+    hand_score = 0
+    hand.each do |card|
+      #binding.pry
+      hand_score += card.value_of_card(card)
+    end
+  hand_score
   end
 
-  def new_score
-    new_hand = initial_hand
-    current_score = new_hand.inject(0){|result,element| result + element}
-  end
+
 end
 ################################################################################
 playing_deck = Deck.new
-binding.pry
+#binding.pry
 player_hand = Hand.new
-binding.pry
+#binding.pry
 dealer_hand = Hand.new
 
-puts "Player was dealt #{card1}"
-puts "Player was dealt #{card2}"
-puts "Player score #{player_hand.initial_score}"
+# setup initial hands
+2.times do
+  playing_deck.deal(player_hand)
+  # binding.pry
+  playing_deck.deal(dealer_hand)
+end
+
+player_hand.cards.each do |card|
+  puts "Player was dealt #{card.rank}#{card.suit}"
+end
+
+puts "Player score #{player_hand.score(player_hand.cards)}"
+
+puts "Hit or Stand (H/S)"
+answer = gets.chomp.upcase
+ # need a prompt to suggest they need to put in a valid input
+  while answer != "S"
+    #binding.pry
+      #binding.pry
+      playing_deck.deal(player_hand)
+      #binding.pry
+      if player_hand.score(player_hand.cards) < 21
+        puts "Player score #{player_hand.score(player_hand.cards)}"
+        puts "Hit or Stand (H/S)"
+        answer = gets.chomp
+      elsif player_hand.score(player_hand.cards) == 21
+        puts "21!"
+      else
+        puts "Bust! Game over..."
+      break
+      end
+   puts "Player score #{player_hand.score(player_hand.cards)}"
+  end
 
 
-# Unless player_hand.score >= 21
-#   puts "Hit or Stand (H/S)"
-#   answer = gets.chomp.uppercase
-#   while answer != "S"
-#     card3 = Card.new
-#     new_hand << card3.value_of_card
-#     puts "Player score #{player_hand.new_score}"
-#     puts "Hit or Stand (H/S)"
-#     answer = gets.chomp
-#   end
-# end
-# while loop, get chomp, prompting for more input
 
+dealer_hand.cards.each do |card|
+  puts "Dealer was dealt #{card.rank}#{card.suit}"
+end
 
+puts "Dealer score #{dealer_hand.score(dealer_hand.cards)}"
+
+  while dealer_hand.score(dealer_hand.cards) < 17
+        playing_deck.deal(dealer_hand)
+        puts "Dealer Hits..."
+          if dealer_hand.score(dealer_hand.cards) > 21
+            puts "You win!"
+            break
+        end
+    end
+
+    if dealer_hand.score(dealer_hand.cards) < 21
+      puts "Dealer stands with a score of #{dealer_hand.score(dealer_hand.cards)}"
+
+    if player_hand.score(player_hand.cards) > dealer_hand.score(dealer_hand.cards)
+          puts "You win!"
+        else
+        "Dealer wins"
+        end
 
 
 
